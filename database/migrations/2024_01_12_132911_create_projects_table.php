1ed035bc6400ebd74d9a569bcc5a71c2d5381780
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-12 15:51:50 +0100

    create and launch migration

--- database/migrations/2024_01_12_132911_create_projects_table.php
+++ database/migrations/2024_01_12_132911_create_projects_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title', 200)->unique();
+            $table->string('slug', 255)->unique();
+            $table->text('description')->nullable();
+            $table->string('url')->nullable();
+            $table->string('technologies', 255);
             $table->timestamps();
         });
     }
