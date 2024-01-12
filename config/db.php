9f6e85c717596fe93035d159b62eabd6831e367d
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-12 15:39:19 +0100

    create db

---
+++ config/db.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    'projects' => [
+        [
+            "title" => "Spotify",
+            "description" => "Questo esercizio è una replica della famosa piattaforma di musica streaming: Spotify Web",
+            "url" => "https://github.com/albertostizzoli/html-css-spotifyweb",
+            "technologies" => "HTML CSS"
+        ],
+        [
+            "title" => "Campo Minato",
+            "description" => "Campo Minato è una simulazione del più celebre gioco di Windows Campo Minato ",
+            "url" => "https://github.com/albertostizzoli/js-campominato-dom",
+            "technologies" => "HTML CSS Javascript"
+        ],
+        [
+            "title" => "Boolzapp",
+            "description" => "Boolzapp è una replica della piattaforma web di messaggistica più utilizzata: WhatsApp Web",
+            "url" => "https://github.com/albertostizzoli/vue-boolzapp",
+            "technologies" => "VueJS"
+        ],
+        [
+            "title" => "Boolflix",
+            "description" => "Boolflix è un semplice motore di ricerca di Film e Serie TV, vagamente ispirato a Netflix.",
+            "url" => "https://github.com/albertostizzoli/vite-boolflix",
+            "technologies" => "VueJS"
+        ],
+        [
+            "title" => "EduPrime",
+            "description" => "Replica della parte grafica del sito EduPrime svolto durante il corso di Boolean",
+            "url" => "https://github.com/albertostizzoli/proj-html-vuejs",
+            "technologies" => "VueJS"
+        ],
+    ]
+];
