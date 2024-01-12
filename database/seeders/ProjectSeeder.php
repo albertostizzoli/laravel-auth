4b5f694f566ca7440a910e6cdb33e6017f8079ed
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-12 16:11:26 +0100

    Create and launch seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 use App\Models\Project;
 
 class ProjectSeeder extends Seeder
@@ -13,6 +14,17 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $projects = config('db.projects');
+        foreach($projects as $project){
+            $newProject = new Project();
+            $newProject->title = $project['title'];
+            $newProject->description = $project['description'];
+            $newProject->url = $project['url'];
+            $newProject->technologies = $project['technologies'];
+            $newProject->user_id = 1;
+            $newProject->slug = Str::slug($project['title'] . '-' . $newProject->id, '-');
+            $newProject->save();
+
+        }
     }
 }
