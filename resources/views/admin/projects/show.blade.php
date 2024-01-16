71f84e9fc697199426734a15c6fdadc8592e3d6c
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-16 08:46:19 +0100

    fixed

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,21 +1,27 @@
 @extends('layouts.app')
 @section('content')
-    <section class="container">
-        <h1>{{ $project->title }}</h1>
-        <p>{{ $project->description}}</p>
-        <p>Tecnologie usate: {{$project->technologies}}</p>
-        <p>Repo GitHub:<a href="{{$project->url}}">{{$project->url}}</a></p>
-        <div>
-            <img class="w-50" src="{{asset('storage/'. $project->image)}}" alt="{{$project->title}}">
-        </div>
-        <button class="btn btn-primary d-inline-block">
-            <a class="text-white text-decoration-none" href="{{ route('admin.projects.edit', $project->id) }}">Modifica</a>
-        </button>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>{{ $project->title }}</h1>
+                <p>{{ $project->description }}</p>
+                <p>Tecnologie usate: {{ $project->technologies }}</p>
+                <p>Repo GitHub:<a href="{{ $project->url }}">{{ $project->url }}</a></p>
+                <div>
+                    <img class="w-50" src="{{ asset('storage/' . $project->image) }}" alt="{{ $project->title }}">
+                </div>
+                <button class="btn btn-primary d-inline-block">
+                    <a class="text-white text-decoration-none"
+                        href="{{ route('admin.projects.edit', $project->id) }}">Modifica</a>
+                </button>
 
-        <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="d-inline">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger cancel-button">Cancella</button>
-        </form>
-        @include('partials.modal_delete')
-    @endsection
+                <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger cancel-button">Cancella</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    @include('partials.modal_delete')
+@endsection
